7cd1e1e9d69f25c8a4ff8ee50f2a6c4b6b028891
jeemee zhang <j.zhang30@share.epsb.ca>
2022-12-05 22:36:32 -0700

    renderer overhaul; gui redesign; resizing still broken; now version 1.1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # a kwik n dirty ascii animation program
 # made with catroidvania
 # first created 2 12 22
-# verison 1.0
+# verison 1.1
 
 # based on the makeyourowntexteditor found on viewsourcecode.org
 # but now its python lol
@@ -19,64 +19,9 @@ from fcntl import ioctl
 # for storing the ioctl return because this is actually ass
 from struct import pack, unpack
 # time
-from time import time_ns
-
-# gets the file number of the terminal this is run in
-unit = stdin.fileno()
-
-# gets the tty attributes
-originalAttributes = termios.tcgetattr(unit)
-newAttributes = termios.tcgetattr(unit)
-# SETTING IFLAGS
-# IXON disabls software flow controls like ctrl s and ctrl q
-# thats a legacy feature apparently
-# ICRNL stops enter from returning a newline
-# BRKINT stops ctrl c and other break characters but they should
-# already have been turned off already lol
-# INPCK is somehting about parity checking which should not affect
-# modern terminals
-# ISTRIP stops the 8th byte form being removed before returning
-# probably already disabled by default but hey why not
-newAttributes[0] &= ~(
-	termios.IXON | termios.ICRNL | termios.BRKINT | termios.INPCK |\
-	termios.ISTRIP
-)
-# SETTING OFLAGS
-# OPOST disables the post proccess of adding a carrige return to a newline
-# note this means we need to specifically input a \r to go back to the start
-# of a line now since the cursor only moves down automatically with OPOST off
-newAttributes[1] &= ~(
-	termios.OPOST
-)
-# SETTING CFLAGS
-# CS8 sets 8 bytes per bit which should be default
-newAttributes[2] |= ~(
-	termios.CS8
-)
-# SETTING LFLAGS
-# ECHO turn off echoing back the char we just typed
-# ICANON turns off canonical mode to raw mode
-# ISIG disables ctrl c and ctrl z
-newAttributes[3] &= ~(
-	termios.ECHO | termios.ICANON |termios.ISIG | termios.IEXTEN
-)
-# SETTING VTIME AND VMIN
-# VMIN sets the minimum amount of bytes to be read before return
-# at 0 it returns as soon as it receives an input
-newAttributes[6][termios.VMIN] = 0
-# VTIME sets the wait time for reading inputs as a number over 10 of a second
-# so at 1 its 1/10 of a second before it moves on to the next line of code
-newAttributes[6][termios.VTIME] = 1
-
-
-def clearBuffer(buf, width=80, height=24, fillchar=" "):
-	# does well, clearing buffer stuff
-	buf = [[fillchar for col in range(width)] for row in range(height)]
-
-	# clear screen and move cursor to top left for drawing
-	buf.insert(0, "\033[?25l\033[H")
-
-	return buf
+from time import time_ns, sleep
+# random (:
+from random import choice
 
 
 def drawBuffer(buf, cx, cy, key, lastkey, contents, frame, framecount,
@@ -93,7 +38,7 @@ def drawBuffer(buf, cx, cy, key, lastkey, contents, frame, framecount,
 
 			# some info
 			title = "{ " + title +\
-				 " | " + str(cx-4) + ":" + str(cy-2) +\
+				 " | " + str(cx) + ":" + str(cy-2) +\
 				 " | " + str(frame) + "/" + str(framecount) + " }"
 
 			# center align
@@ -104,38 +49,107 @@ def drawBuffer(buf, cx, cy, key, lastkey, contents, frame, framecount,
 			# keypressed
 		elif row == 2:
 			title = "[ " + lastkey + " ]"
-			buf[row][:] = "=" * ((width-len(title))//2) + title +\
+			buf[row][0:] = "=" * ((width-len(title))//2) + title +\
 				"=" * ((width-len(title))//2)
 		elif row == height:
 			buf[row] = "=" * width
 		else:
-			num = str(row-2)
+# dont need line numbers
+#			num = str(row-2)
 			# some border decor
-			if len(num) < 2:
-				buf[row][1:3] = num + " " * (2 - len(num))
-			elif len(num) > 2:
-				buf[row][1:3] = num[-2:]
-			else:
-				buf[row][1:3] = num
-			buf[row][3] = "|"
+#			if len(num) < 2:
+#				buf[row][1:3] = num + " " * (2 - len(num))
+#			elif len(num) > 2:
+#				buf[row][1:3] = num[-2:]
+#			else:
+#				buf[row][1:3] = num
+#			buf[row][3] = "|"
+			buf[row][1] = "["
 
 			# contents
-			buf[row][4:] = contents[row-2]
+			buf[row][2:] = contents[row-2]
 
 		# draw cursor
-		if row == cy+1: buf[row][cx]= "\033[7m"+contents[row-2][cx-4]+"\033[0m"
+		if row == cy: buf[row][cx]= "\033[7m"+contents[row-2][cx]+"\033[0m"
 
 		# moves everything down unless its the last row
 		if row < height - 1: buf[row].append("\r\n")
 	return buf
 
 
-def drawScreen(unitno, buf):
+def clearCanvas(width=80, height=24, fillchar=" "):
+	# does well, clearing buffer stuff
+	return [
+		[fillchar for col in range(width)] for row in range(height)]
+
+
+def drawToCanvas(canvas, x, y, content, canvasWidth=None, contentWidth=None):
+	# draws individual elements to the buffer
+
+	# if none are supplied
+	if not canvasWidth: canvasWidth = len(canvas[0])-1
+	if not contentWidth: contentWidth = len(content[0])-1
+
+	# so we dont need to calculate on every iter
+	canvasHeight = len(canvas)-1
+	contentHeight = len(content)-1
+
+	# draw for each line
+	for row,line in enumerate(canvas):
+		# so we dont off the bottom
+		if row + y > canvasHeight: break
+		# so we dont try and index nonexistant lines
+		if row > contentHeight: break
+		# so we dont draw off the side
+		if contentWidth > canvasWidth:
+			canvas[row + y][x:] = content[row][:canvasWidth-x]
+		else:
+			canvas[row + y][x:] = content[row]
+
+	return canvas
+
+
+def printToCanvas(canvas, x, y, string, canvasWidth=None):
+	# print a string to the canvas
+
+	# if none are supplied
+	if not canvasWidth: canvasWidth = len(canvas[0])-1
+
+	# write to buffer row
+
+	# so we dont draw off the edge
+	if len(string) > canvasWidth:
+		canvas[y][x:] = string[:canvasWidth-x]
+	else:
+		canvas[y][x:] = string
+
+	return canvas
+
+
+def drawCursor(canvas, x, y):
+	# draws the cursor as inverted text
+	canvas[y][x] = "\033[7m" + canvas[y][x] + "\033[0m"
+
+	return canvas
+
+
+def drawScreen(unitno, canvas, height=None):
 	# draws the buffer to screen
+
+	# if none if supplied
+	if not height: height = len(canvas)-1
+
+	# putting it all in one string
 	try:
-		stringbuf = ""
-		for row in buf:
-			stringbuf += "".join(row)
+		# add clear screen, move to top of screen
+		stringbuf = "\033[H\033[?25l"
+		for row,line in enumerate(canvas):
+			# add clear line char and content
+			if row > 0: stringbuf += "\033[K"
+			# add buffer row
+			stringbuf += "".join(line)
+			# add a newline
+			if row < height: stringbuf += "\r\n"
 
 		os.write(unitno, stringbuf.encode("utf-8"))
 	except Exception as exc:
@@ -157,6 +171,12 @@ def handleInput(key):
 	if key == b"2B": return "SHIFTDOWNARROW"
 	if key == b"2C": return "SHIFTRIGHTARROW"
 	if key == b"2D": return "SHIFTLEFTARROW"
+	# ctrl shift arrow keys omg
+	if key == b"6A": return "CTRLSHIFTUPARROW"
+	if key == b"6B": return "CTRLSHIFTDOWNARROW"
+	if key == b"6C": return "CTRLSHIFTRIGHTARROW"
+	if key == b"6D": return "CTRLSHIFTLEFTARROW"
+
 
 	# control characters
 
@@ -206,7 +226,7 @@ def handleInput(key):
 def resizeAnimation(project, width, height):
 	# a quick function to make sure a project is the right size
 	correctsize = [[
-		[" " for col in range(width-3)] for row in range(height-1)]
+		[" " for col in range(width)] for row in range(height)]
 		for f in range(len(project))]
 
 	for f,fr in enumerate(project):
@@ -229,7 +249,7 @@ def saveAnimation(frames, filename=None, width=80, height=24, linesep="@@"):
 		stringframe = ""
 		for f,row in enumerate(frame):
 			if f > height: break
-			if f == 0: continue
+			#if f == 0: continue
 			stringframe += "".join(row[:width]) + "\n"
 		if stringframe: stringframes += stringframe + linesep + "\n"
 
@@ -255,34 +275,86 @@ def loadAnimation(filename, linesep="@@"):
 	return contents
 
 
+# main program code
 try:
-	# move the variable stuff up top into here???
+
+	# version inso
+	version = 1.1
+
+	# gets the file number of the terminal this is run in
+	unit = stdin.fileno()
+
+	# gets the tty attributes
+	originalAttributes = termios.tcgetattr(unit)
+	newAttributes = termios.tcgetattr(unit)
+	# SETTING IFLAGS
+	# IXON disabls software flow controls like ctrl s and ctrl q
+	# thats a legacy feature apparently
+	# ICRNL stops enter from returning a newline
+	# BRKINT stops ctrl c and other break characters but they should
+	# already have been turned off already lol
+	# INPCK is somehting about parity checking which should not affect
+	# modern terminals
+	# ISTRIP stops the 8th byte form being removed before returning
+	# probably already disabled by default but hey why not
+	newAttributes[0] &= ~(
+		termios.IXON | termios.ICRNL | termios.BRKINT | termios.INPCK |\
+		termios.ISTRIP
+	)
+	# SETTING OFLAGS
+	# OPOST disables the post proccess of adding a carrige return to a newline
+	# note this means we need to specifically input a \r to go back to the start
+	# of a line now since the cursor only moves down automatically with OPOST off
+	newAttributes[1] &= ~(
+		termios.OPOST
+	)
+	# SETTING CFLAGS
+	# CS8 sets 8 bytes per bit which should be default
+	newAttributes[2] |= ~(
+		termios.CS8
+	)
+	# SETTING LFLAGS
+	# ECHO turn off echoing back the char we just typed
+	# ICANON turns off canonical mode to raw mode
+	# ISIG disables ctrl c and ctrl z
+	newAttributes[3] &= ~(
+		termios.ECHO | termios.ICANON |termios.ISIG | termios.IEXTEN
+	)
+	# SETTING VTIME AND VMIN
+	# VMIN sets the minimum amount of bytes to be read before return
+	# at 0 it returns as soon as it receives an input
+	newAttributes[6][termios.VMIN] = 0
+	# VTIME sets the wait time for reading inputs as a number over 10 of a second
+	# so at 1 its 1/10 of a second before it moves on to the next line of code
+	newAttributes[6][termios.VTIME] = 1
+
 	# sets the new tty attributesw
 	termios.tcsetattr(unit, termios.TCSAFLUSH, newAttributes)
 
-	# gets some terminal info
+	# gets terminal width/height in characters
 	# NOTE ioctl does not work on all systems but fuck that im not
 	# doing the hard way with escape sequences \\:
 	# i really do be chasing modules huh
 	terminfo = ioctl(unit, termios.TIOCGWINSZ, pack("HHHH", 0,0,0,0))
-	# a tupe containing: (rows, columns, xpixels, ypixels)
-	# we really dont need the pixel sizes but hey why not
+	# a tupe containing: (rows, columns, x pixels, y pixels)
+	# dont need pixels tho
 	terminfo = unpack("HHHH", terminfo)
 	heightChar = terminfo[0]
 	widthChar = terminfo[1]
 
+	resize = True
+
 	# declare sum more stuff
 	cursorx = 0
-	cursory = 0
-	cursorlastx = 0
-	cursorlasty = 0
+	cursory = 1
+	cursorlastx = 1
+	cursorlasty = 1
 
 	# buffer list
 	buf = []
 
 	# canvas
-	frames = [[
-		[" " for col in range(widthChar-3)] for row in range(heightChar-1)]]
+	frames = [clearCanvas(widthChar, heightChar, " ")]
 	frame = 0
 
 	# save last character
@@ -290,14 +362,31 @@ try:
 
 	# command interface
 	terminal = False
-	terminalframe = [
-		[" " for col in range(widthChar-3)] for row in range(heightChar-1)]
+	terminalframe = clearCanvas(widthChar, heightChar, " ")
 
 	# savig / loading
 
+	# random stuff
+	decorchar = "::"
+	#choice(["!", "@", "#", "$", "%", "&", "=", ":"])
+
 	# main program loop
 	while True:
-		# reads one byte from the standard input
+
+		# reget window size
+		if resize:
+			terminfo = ioctl(unit, termios.TIOCGWINSZ, pack("HHHH", 0,0,0,0))
+			terminfo = unpack("HHHH", terminfo)
+			heightChar = terminfo[0]
+			widthChar = terminfo[1]
+
+		# cursor limits
+		cursorxmin = 0
+		cursorxmax = widthChar-1
+		cursorymin = 1
+		cursorymax = heightChar-1
+
+		# reads at least 1 byte from the standard input
 		rawchar = os.read(unit, 4)
 		# keeps rawchar in case i need to debug with it
 		char = handleInput(rawchar)
@@ -311,19 +400,17 @@ try:
 			if len(frames) > 1: frames.pop(frame)
 		# append frame
 		elif char == "^\\" and lastchar == "^\\" and not terminal:
-			frames.append([
-						[" " for col in range(widthChar-4)]
-							for row in range(heightChar-1)])
+			frames.append(clearCanvas(widthChar, heightChar, " "))
 		# insert frame
 		elif char == "^J" and lastchar == "^J" and not terminal:
-			frames.insert(0 if frame == 0 else frame-1, [
-						[" " for col in range(widthChar-4)]
-							for row in range(heightChar-1)])
+			frames.insert(frame, clearCanvas(widthChar, heightChar, " "))
 			frame -= 1
 		elif char == "^K" and lastchar == "^K" and not terminal:
-			frames.insert(-1 if frame == len(frames)-1 else frame+1, [
-						[" " for col in range(widthChar-4)]
-							for row in range(heightChar-1)])
+			if len(frames)-1 == frame:
+				frames.append(clearCanvas(widthChar, heightChar, " "))
+			else:
+				frames.insert(frame+1,
+				clearCanvas(widthChar, heightChar, " "))
 			frame += 1
 		# duplicate frame
 		elif char == "L" and lastchar == "^L" and not terminal:
@@ -360,15 +447,24 @@ try:
 		elif char == "SHIFTLEFTARROW":
 			cursory -= 1
 			cursorx -= 2
+		# fast movement
+		elif char == "CTRLSHIFTUPARROW":
+			cursory -= 4
+		elif char == "CTRLSHIFTDOWNARROW":
+			cursory += 4
+		elif char == "CTRLSHIFTRIGHTARROW":
+			cursorx += 4
+		elif char == "CTRLSHIFTLEFTARROW":
+			cursorx -= 4
 		# tab
 		elif char == "^I":
 			cursorx += 4
 		# backspace
 		elif char == "^?":
 			if terminal:
-				terminalframe[cursory-1][cursorx-5] = " "
+				terminalframe[cursory-1][cursorx-1] = " "
 			elif not terminal:
-				frames[frame][cursory-1][cursorx-5] = " "
+				frames[frame][cursory-1][cursorx-1] = " "
 			if cursorx == 1: cursory -= 1
 			cursorx -= 1
 		# enter
@@ -377,18 +473,18 @@ try:
 			cursorx = 0
 		# jump to borders
 		elif char == "^Z":
-			cursorx = 0
+			cursorx = cursorxmin
 		elif char == "^V":
-			cursorx = widthChar
+			cursorx = cursorxmax
 		elif char == "^X":
-			cursory = 0
+			cursory = cursorymin
 		elif char == "^C":
-			cursory = heightChar
+			cursory = cursorymax
 		# center cursor
 		elif char == "^B":
-			cursorx = widthChar//2
+			cursorx = cursorxmax//2
 		elif char == "^N":
-			cursory = heightChar//2
+			cursory = cursorymax//2
 		# frame change
 		elif char == "^[" and not terminal:
 			frame -= 1
@@ -405,7 +501,7 @@ try:
 				cursorlastx = cursorx
 				cursorlasty = cursory
 				cursorx = 0
-				cursory = 0
+				cursory = 1
 		# run command
 		# should move this into a function
 		elif char == "^E":
@@ -426,31 +522,35 @@ try:
 						terminalframe[cursory][0:]= "Not implemented yet lol!"
 				except Exception as exc:
 					terminalframe[cursory][0:] = str(exc)
-		# hotkeys
-		# todo
 		# draw if nothing else
 		elif len(char) == 1:
 			if terminal:
-				terminalframe[cursory-1][cursorx-4] = char[0]
+				terminalframe[cursory-1][cursorx] = char[0]
 			elif not terminal:
-				frames[frame][cursory-1][cursorx-4] = char[0]
+				frames[frame][cursory-1][cursorx] = char[0]
 			cursorx += 1
 
+		# not even sure if this is needed since only shift arrowkeys and tab
+		# move more than 1 at a time
 		# clamp cursor position within screen bounds
-		if cursory < 1: cursory = 2
-		if cursory > heightChar - 1: cursory = heightChar - 2
-		if cursorx > widthChar + 1: cursorx = widthChar
-		if cursorx < 3: cursorx = 4
+		if cursory < cursorymin-1:
+			cursory = cursorymin
+		elif cursory > cursorymax+1:
+			cursory = cursorymax
+		elif cursorx < cursorxmin-1:
+			cursorx = cursorxmin
+		elif cursorx > cursorxmax+1:
+			cursorx = cursorxmax
 
 		# wraparound at screen edge
-		if cursory == 1:
-			cursory = heightChar - 2
-		elif cursory == heightChar - 1:
-			cursory = 2
-		if cursorx == widthChar + 1:
-			cursorx = 4
-		elif cursorx == 3:
-			cursorx = widthChar
+		if cursory == cursorymin-1:
+			cursory = cursorymax
+		elif cursory == cursorymax+1:
+			cursory = cursorymin
+		if cursorx == cursorxmin-1:
+			cursorx = cursorxmax
+		elif cursorx == cursorxmax+1:
+			cursorx = cursorxmin
 
 		# frame move wrap
 		if frame < 0:
@@ -459,21 +559,44 @@ try:
 			frame = 0
 
 		# empty buffer
-		buf = clearBuffer(buf, widthChar, heightChar, fillchar=" ")
+		buf = clearCanvas(widthChar, heightChar, fillchar=" ")
 
-		# draw to screen
+		# draw title bar
 		if terminal:
-			buf = drawBuffer(buf,
-				cursorx, cursory,
-				char, lastchar,
-				terminalframe, "X", "X",
-				widthChar, heightChar, title="COMMAND MODE")
-		elif not terminal:
-			buf = drawBuffer(buf,
-				cursorx, cursory,
-				char, lastchar,
-				frames[frame], frame+1, len(frames),
-				widthChar, heightChar, title="EDITING")
+			editmode = "COMMAND MODE"
+		else:
+			editmode = "EDIT MODE"
+
+		title = decorchar + "[ CATSCII v" + str(version) + " ][ " +\
+		editmode + " ][ FRAME " + str(frame+1) + "/" + str(len(frames)) +\
+		" ][ CURSOR " + str(cursorx+1) + ":" + str(cursory) +\
+		" ][ " + lastchar + " ]"
+		title += "=" * (widthChar-len(title))
+		buf = printToCanvas(buf, 0, 0, title, widthChar)
+
+		# draw contents
+		if terminal:
+			buf = drawToCanvas(buf, 0, 1, terminalframe)
+		else:
+			buf = drawToCanvas(buf, 0, 1, frames[frame])
+
+		# draw cursor
+		buf = drawCursor(buf, cursorx, cursory)
+
+# OLD
+		# draw to screen
+#		if terminal:
+#			buf = drawBuffer(buf,
+#				cursorx, cursory,
+#				char, lastchar,
+#				terminalframe, "X", "X",
+#				widthChar, heightChar, title="COMMAND MODE")
+#		elif not terminal:
+#			buf = drawBuffer(buf,
+#				cursorx, cursory,
+#				char, lastchar,
+#				frames[frame], frame+1, len(frames),
+#				widthChar, heightChar, title="EDITING")
 
 		# draw the whole buffer
 		drawScreen(unit, buf)
